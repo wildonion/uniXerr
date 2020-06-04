e241544aad5b90714413cef7100c90e8405d25a4
2017-A-15 <ea_pain@yahoo.com>
2020-06-05 01:28:34 +0430

    added eye.py for controlling the whole uniXerr server

---
+++ eye.py
@@ -0,0 +1,43 @@
+
+
+# coding: utf-8
+
+'''
+	Codded By : 
+
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+
+
+_________________________________________________________________
+TODO : 
+
+	https://github.com/fawkesley/pub-sub-python-zeromq-protobuf
+
+only this script can run the uniXerr protocol using zmq.
+enable authentication for this file and uniXerr app server
+_________________________________________________________________
+
+'''
+import zmq
+
+
+context = zmq.Context()
+print("Connecting to uniXerr server")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+
+for request in range(10):
+	print(f"[+] Sending request {request}")
+	
+	# send your cnc through socket to app.py
+	# ...
+
+	socket.send(b"Hello")
+
+	message = socket.recv()
+	print(f"[+] Received reply {request}, {message}")
