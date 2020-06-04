acb3cb1c678d92e12c98e267f8859916f6e8f0fa
2017-A-15 <ea_pain@yahoo.com>
2020-06-05 01:20:00 +0430

    added coin generation for uniXerr AI kernel core

---
+++ core/kernel/coin_generation/model.py
@@ -0,0 +1,55 @@
+
+
+# coding: utf-8
+
+'''
+	Codded By : 
+
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+
+
+
+ -------------------------
+|  coin generation model
+| ------------------------
+| generate coins based on positions
+| and other features using 
+| CVAE (Conditional Variational Autoencoder)
+|
+| minimum number of coins for everyone generated 
+| by uinXerr protocol is 10, because the minimum 
+| of each feature is 5.
+|
+|
+
+'''
+
+import os
+import sys
+import pandas as pd
+import torch
+from torch import nn
+from torch.autograd import Variable
+# from pytorch_lightning.core.lightning import LightningModule
+
+
+# class CoinGenerationModel(LightningModule):
+# 	def __init__(self, device):
+# 		super().__init__()
+
+# 	def forward(self):
+# 		pass
+
+# 	def training_step(self):
+# 		pass
+
+
+# 	def configure_optimizers(self):
+# 		pass
+
+# 	def train_dataloader(self):
+# 		pass
