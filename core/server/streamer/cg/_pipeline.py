4dbdd2db38b904a5ed3defe8389e0af7078445b9
2017-A-15 <ea_pain@yahoo.com>
2020-06-05 01:28:09 +0430

    added online training requirement files over streaming for coin generation + fake dataset loader files

---
+++ core/server/streamer/cg/_pipeline.py
@@ -0,0 +1,30 @@
+
+
+
+
+# coding: utf-8
+
+'''
+	Codded By : 
+
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+
+'''
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import preprocessing
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+
+class pipline(Dataset):
+	def __init__(self):
+		pass
