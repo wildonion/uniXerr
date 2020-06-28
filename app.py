e7b9c94ab56c96e801e7d5d32d939a4e8b743f10
2017-A-15 <ea_pain@yahoo.com>
2020-06-29 02:30:07 +0430

    add kafka streaming section and run uvicorn in dev mode and a little touch for worker arg

--- app.py
+++ app.py
@@ -13,38 +13,43 @@
 ░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
 ░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
 
-
 '''
 
 
 import uvicorn
 import typer
 import subprocess
+import multiprocessing
 from controller import app
 from server.api import api
 
 
+
 @app.command()
-def deploy(build: bool = typer.Option(False, "--build", help="Building for production.")):
+def deploy(build: bool = typer.Option(False, "--build", help="Building for production."),
+		   streamer: bool = typer.Option(False, "--kafka", help="Streamer processor for online training.")
+		   ):
 	typer.echo("\n________Production process________\n")
+
 	if build:
 		typer.echo("\t---building with docker\n")
+		# TODO : build api.py inside server folder using docker and traefik
+		# ...
+
+
+	if streamer:
+		typer.echo("\t---streaming over kafka for online training\n")
+		# TODO : work on streamer.py inside server folder
+		# ...
+
 
 
 @app.command()
-def develop(workers: int = typer.Option(4, help="Number of workers.", min=4),
-			asgi_server: str = typer.Option('gunicorn', help="ASGI server. uvicorn or gunicorn")):
+def develop(workers: int = typer.Option(4, help="Number of workers.", min=4)):
 
 	typer.echo("\n________Running in development________\n")
-	
-	if asgi_server == 'uvicorn':
-		uvicorn.run('app:api', host="127.0.0.1", port=8000, reload=True, workers=workers)
-	
-	else: # linux only
-		cmd = f"gunicorn app:api -w {workers} -k uvicorn.workers.UvicornWorker"
-		subprocess.run([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-
+	workers = number_of_workers() if workers != 4 else workers
+	uvicorn.run('app:api', host="127.0.0.1", port=8000, reload=True, workers=workers)
 
 
 if __name__ == "__main__":
