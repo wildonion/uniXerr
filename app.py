5cf446946000f9d1b1517835f4ccbb16728e2e75
2017-A-15 <ea_pain@yahoo.com>
2020-07-03 21:31:16 +0430

    add controller path to sys for typer cli __ import api directly from server folder

--- app.py
+++ app.py
@@ -15,13 +15,22 @@
 
 '''
 
+import os
+import sys
+
+# typer doesn't know the path of imported module in where it's imported like in here
+# to solve this we have to give typer the full path of a module that we want to import it in its main file(app.py).
+# it's like typer has its own namespace for running functions decorated with app.command()
+# this file will import in typer and you can see the bytecode of this file in __pycache__ folder.
+configfile = 'controller.py'
+path = sys.path.append(os.path.dirname(os.path.expanduser(configfile)))
 
 import uvicorn
 import typer
 import subprocess
 import multiprocessing
 from controller import app
-from server.api import api
+from server import api
 
 
 
