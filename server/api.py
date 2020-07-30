3e12ce957fda6a8e12e207d14ec5ae44b3824119
2017-A-15 <ea_pain@yahoo.com>
2020-07-30 11:06:39 +0430

    fix some bugs

--- server/api.py
+++ server/api.py
@@ -15,7 +15,13 @@
 
 
 TODO : create a csv api of all students features from database 
-		and save it in dataset folder as pc_features.csv 
+		and save it in dataset folder as pc_features.csv,
+		first load the pc_classified_positions.csv into database.
+
+
+TODO : db query for fetching all info about
+		students positions and marks.
+
 
 TODO : use mongodb and graphql for req, res ops
 
@@ -42,7 +48,8 @@ async def read_root():
 # load the labeled csv file and return the position for a user_id
 @api.get("/credit-info/{user_id}")
 async def get_position(user_id: int):
-	csv_path = os.path.dirname(os.path.abspath(__file__)) + '/dataset/pc_features_labeled.csv'
+	# TODO : fetch the position from db based on user_id
+	csv_path = os.path.dirname(os.path.abspath(__file__)) + '/dataset/pc_features_labeled-latent.csv'
 	df = pd.read_csv(csv_path)
 	rec = df.loc[df['user_id'] == user_id]
 	return {"position": rec['position'].values[0]}
