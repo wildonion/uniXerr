eee0c931dff3ac054c415b5b9318b7afa408259d
2017-A-15 <ea_pain@yahoo.com>
2020-07-31 19:09:06 +0430

    work in progress

--- core/games/agents/csdigger/codeforces/brackets.py
+++ core/games/agents/csdigger/codeforces/brackets.py
@@ -12,27 +12,43 @@
 import sys
 
 
+min_ops = 0
+open_stack = []
+close_stack = []
+
+
+def move_ith(s):
+	global min_ops
+
+
 def is_reg(s):
-	if len(s) % 2 == 0:
-		open_brack  = s[:int(len(s)/2)]
-		close_brack = s[int(len(s)/2):]
-		
-		for i in range(len(open_brack)):
-			if open_brack[i] != "(":
-				print("not reg brack")
-				sys.exit(1)
-
-		for j in range(len(close_brack)):
-			if close_brack[j] != ")":
-				print("not reg brack")
-				sys.exit(1)
-
+	global open_stack, close_stack
+	for i in range(1, len(s)):
+		if s[i] == "(":
+			open_stack.append(s[i])
+		if s[i] == ")":
+			if i == 0:
+				return False
+				break
+			else:
+				close_stack.append(s[i])
+	if len(open_stack) == len(close_stack):
+		return True
 	else:
-		print("the length must be even!")
-		sys.exit(1)
+		return False
 
 
-
-_input = "( () ) ()"
-s = _input.replace(" ", "")
-is_reg(_input)
+test_cases, i = int(input("test cases >>> ")), 0
+while i < test_cases < 2000:
+	_input = input()
+	s = _input.replace(" ", "")
+	if 2 <= len(s) <= 50:
+		if len(s) % 2 == 0:
+			if not is_reg(s):
+				print("making it reg...")
+				move_ith(s)
+		else:
+			print("the length must be even!")
+			sys.exit(1)
+	print("=============")
+	i+=1
