2d9b7b66691102c9db874c0f5b94116271742c67
wildonion <marefimoghaddam@unixerr.com>
2020-10-10 12:50:43 +0330

    next section will smash the api server algorithm

--- server/api.py
+++ server/api.py
@@ -16,14 +16,15 @@
 
 TODO : create a csv api of all students features from database 
 		and save it in dataset folder as pc_features.csv,
-		first load the pc_classified_positions.csv into database.
+		first load the input_data_classified.csv into database
+		as positions document -> |user_id|position|
 
 
 TODO : db query for fetching all info about
 		students positions and marks.
 
 
-TODO : use mongodb and graphql for req, res ops
+TODO : use mongodb and graphql for req and res ops, use DevOps tools
 
 '''
 
@@ -43,13 +44,10 @@ async def read_root():
 	return {"Welcome to": "uniXerr"}
 
 
-
-
-# load the labeled csv file and return the position for a user_id
 @api.get("/credit-info/{user_id}")
 async def get_position(user_id: int):
 	# TODO : fetch the position from db based on user_id
-	csv_path = os.path.dirname(os.path.abspath(__file__)) + '/dataset/pc_features_labeled-latent.csv'
+	csv_path = os.path.dirname(os.path.abspath(__file__)) + '/dataset/input_data_classified_positions.csv'
 	df = pd.read_csv(csv_path)
 	rec = df.loc[df['user_id'] == user_id]
 	return {"position": rec['position'].values[0]}
