8e9296bc7dcb43748c91547455aa0a882fca28ed
wildonion <marefimoghaddam@unixerr.com>
2020-10-10 12:43:43 +0330

    neural networks model for position classification algorithm

--- core/position_classification/_nn.py
+++ core/position_classification/_nn.py
@@ -16,9 +16,16 @@
  --------------------------------------------
 |        Position Classification model
 | -------------------------------------------
-| classify positions based on 
-| clustered dataset.
+| neural network classifer
+| for position classification
 |
+|
+| probability of position A : p_a
+| probability of position B : p_b
+| probability of position C : p_c
+| probability of position D : p_d
+| probability of position E : p_e
+| probability of position F : p_f
 |
 
 '''
@@ -32,5 +39,14 @@ from torch.autograd import Variable
 
 
 class Position(nn.Module):
-	def __init__(self):
-		pass
+	def __init__(self, input_neurons, output_neurons):
+		super(Position, self).__init__()
+		self.i_l = nn.Linear(input_neurons, 16) # i_l.weight  : torch.Size([16, 4]), is transposed because of back-propagation algorithm
+		self.o_l = nn.Linear(16, output_neurons) # o_l.weight : torch.Size([6, 16]), is transposed because of back-propagation algorithm
+		self.relu = nn.ReLU()
+		self.softmax = nn.Softmax(dim=1) # dim=1, sum of each rows will be 1 : p_a + p_b + p_c + p_d + p_e + p_f
+
+	def forward(self, x):
+		output = self.relu(self.i_l(x))
+		output = self.softmax(self.o_l(output))
+		return output
