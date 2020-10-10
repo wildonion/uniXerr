a7eb507a4f233e994a01755a4f8f7699a865d3dd
wildonion <marefimoghaddam@unixerr.com>
2020-10-10 12:44:29 +0330

    a predictor class for inputs prediction using pre-trained classifier model

--- core/position_classification/classifier.py
+++ core/position_classification/classifier.py
@@ -18,17 +18,123 @@
  --------------------------------------------
 |             Position Classifier
 | -------------------------------------------
-| save positions using
-| a pre-trained model. 
+| predict and save positions using
+| a pre-trained model.
+|
+|
+|
+| self.model                 : classifier pre-trained model
+| self.__save                : save prediction on input data into a csv file
+| self.__load                : load pre-trained classifier model
+| self.predict               : predict output on input data
+| self.__call__ 			 : make a prediction on a input data
+|
 |
 |
 
-
-# TODO : save predicted positions in a new csv file called pc_classified_positions.csv
-# TODO : log everything in tensorboard
 
 '''
 
-class Predictor:
-	def __init__(self):
-		pass
+
+from sklearn import preprocessing
+from ._nn import Position 
+import matplotlib.pyplot as plt
+import sys
+import os
+import pathlib
+import pandas as pd
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+
+class predictor:
+	def __init__(self, device, data_type):
+		self.__model_path = os.path.dirname(os.path.abspath(__file__)) + f'/utils/pc_model_classifier-{data_type}.pth'
+		self.__input_data = None
+		self.__positions = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F'}
+		self.__data_type = data_type
+		self.model = None
+
+		cuda = torch.cuda.is_available() if device is 'cuda' else None
+		self.__device = torch.device("cuda" if cuda else "cpu")
+		torch.backends.cudnn.benchmark = True
+
+
+		if os.path.exists(self.__model_path):
+			print(f"\n________found existing pre-trained classifier model trained on {self.__data_type} data for prediction________\n")
+			self.__load()
+		else:
+			print(f"\t➢   something went wrong with loading pre-trained classifier model, please make sure there is one at {self.__model_path}")
+			sys.exit(1)
+
+
+	def __load(self):
+
+		input_features = 4
+		output_features = 6
+
+		try:
+			checkpoint = torch.load(self.__model_path)
+			print(f"\t➢   loaded pre-trained model from {self.__model_path}\n")
+		except IOError:
+			print(f"\t➢   can't load pre-trained model from : {self.__model_path}\n")
+
+
+		self.model = Position(input_neurons=input_features, output_neurons=output_features).to(self.__device)
+		self.model.load_state_dict(checkpoint['model_state_dict'])
+		self.optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-3, momentum=0.9)
+		self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+		self.epoch = checkpoint['epoch']
+		self.training_loss = checkpoint['training_loss']
+		self.training_best_accuracy = checkpoint['training_best_accuracy']
+		self.loss_tracker = checkpoint['loss_tracker']
+		self.model.eval()
+
+
+	def __save(self, predictions):
+		curr_dir = os.path.dirname(os.path.abspath(__file__))
+		input_data = os.path.dirname(os.path.abspath(__file__))+'/utils/input_data.csv'
+		input_data_classified = os.path.abspath(curr_dir + f"/../../server/dataset/input_data_classified_positions_using-pre-trained_model_on-{self.__data_type}.csv")
+		numpy_predictions = predictions.detach().numpy()
+		Df = pd.read_csv(input_data)
+		Df['position'] = np.array(list(self.__positions[pred] for pred in numpy_predictions))
+		Df.to_csv(input_data_classified, index=False)
+		print(f"\t➢   new dataset saved in {input_data_classified}\n")
+
+
+	def __predict(self):
+		if self.__input_data is not None:
+			print(f"\n________predicting on input data using pre-trained classifier on {self.__device}________\n")
+			_input = Variable(torch.from_numpy(self.__input_data), requires_grad=False)
+			_input = _input.to(self.__device)
+			outputs = self.model(_input.float())
+			predictions = outputs.argmax(dim=1)
+			self.__save(predictions)
+		else:
+			print(f"[?] can't predict on a None data object.")
+			sys.exit(1)
+
+
+	def __call__(self, *args):
+		for arg in args:
+			if type(arg) is pathlib.PosixPath:
+				if os.path.dirname(arg):
+					df = pd.read_csv(arg)
+					df.drop('user_id', axis=1, inplace=True)
+					input_data = df.to_numpy()
+					self.__input_data = preprocessing.StandardScaler().fit_transform(input_data)
+					self.__predict()
+				else:
+					print(f"[?] make sure there is csv file at {arg}.")
+					sys.exit(1)	
+			elif arg is None:
+				print(f"[?] data object can't be None.")
+				sys.exit(1)
+			elif type(arg) is np.ndarray:
+				self.__input_data = preprocessing.StandardScaler().fit_transform(arg)
+				self.__predict()
+			else:
+				print(f"[?] please specify a numpyndarray data object or a csv path of data for prediction.")
+				sys.exit(1)
+		
