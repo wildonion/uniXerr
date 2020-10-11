76ee2c4e00983c47247748d18d2c3e5830bfa211
wildonion <marefimoghaddam@unixerr.com>
2020-10-11 16:30:55 +0330

    working on building credit-info api server using apache cassandra db

--- server/api.py
+++ server/api.py
@@ -13,19 +13,6 @@
 ░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
 
 
-
-TODO : create a csv api of all students features from database 
-		and save it in dataset folder as pc_features.csv,
-		first load the input_data_classified.csv into database
-		as positions document -> |user_id|position|
-
-
-TODO : db query for fetching all info about
-		students positions and marks.
-
-
-TODO : use mongodb and graphql for req and res ops, use DevOps tools
-
 '''
 
 
@@ -37,22 +24,14 @@ import os
 api = FastAPI()
 
 
-
 # root of the api
 @api.get("/")
 async def read_root():
 	return {"Welcome to": "uniXerr"}
 
 
+# credit-info api
 @api.get("/credit-info/{user_id}")
 async def get_position(user_id: int):
-	# TODO : fetch the position from db based on user_id
-	csv_path_latent = os.path.dirname(os.path.abspath(__file__)) + '/dataset/input_data_classified_positions_using-pre-trained_model_on-latent.csv'
-	df_latent = pd.read_csv(csv_path_latent)
-	rec_latent = df.loc[df['user_id'] == user_id]
-	
-	csv_path_raw = os.path.dirname(os.path.abspath(__file__)) + '/dataset/input_data_classified_positions_using-pre-trained_model_on-raw.csv'
-	df_raw = pd.read_csv(csv_path_raw)
-	rec_raw = df.loc[df['user_id'] == user_id]
-	
-	return {"pre-trained on latent": {"position": rec_latent['position'].values[0]}, "pre-trained on raw": {"position": rec_raw['position'].values[0]}}
+	# TODO : fetch features from cassandra db based on user_id
+	return {"rollcall_score": None, "class_activity": None, "discipline": None, "total_quizzes_avg": None, "positions": None}
