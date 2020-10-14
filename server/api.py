3f7a3adaafd50b9af2ecc4f02e27f9fc6dd75996
wildonion <marefimoghaddam@unixerr.com>
2020-10-14 23:06:20 +0330

    working on credit-info api __ initialize db and schemas

--- server/api.py
+++ server/api.py
@@ -15,13 +15,17 @@
 
 '''
 
-
+from .db import init
+from .db.schema import Users, Positions
 from fastapi import FastAPI
 import pandas as pd
 import os
 
 
 api = FastAPI()
+db = init()
+user_schema = Users(db.session)
+position_schema = Positions(db.session)
 
 
 # root of the api
@@ -33,5 +37,13 @@ async def read_root():
 # credit-info api
 @api.get("/credit-info/{user_id}")
 async def get_position(user_id: int):
-	# TODO : fetch features from cassandra db based on user_id
-	return {"rollcall_score": None, "class_activity": None, "discipline": None, "total_quizzes_avg": None, "positions": None}
+	# TODO : first insert all classified input data positions into users table with columns position_latent and position_raw
+	# 		 then fetch a classified student with his/her feature from the table
+	# ...
+	users = user_schema.get()
+	for user in users:
+		print(user)
+	return {"rollcall_score": user.rollcall_score, "class_activity": user.class_activity, 
+			"discipline": user.discipline, "total_quizzes_avg": user.total_quizzes_avg, 
+			"positions": {"latent": user.position_latent, "raw": user.position_raw}}
+		  
