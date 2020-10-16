e4d8b89ffd117db650ea4bdc5e0e0d04e4dcaba2
wildonion <marefimoghaddam@unixerr.com>
2020-10-17 00:57:15 +0330

    setup db connection and initialization ___ add query method

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -13,27 +13,43 @@
 
 '''
 
+from cassandra.io.libevreactor import LibevConnection
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine import connection
+from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from dotenv import load_dotenv, find_dotenv
+from .schema import User
 import os
 import typer
 
 
 load_dotenv(find_dotenv())
-HOSTS  = os.getenv("HOST").split(",")
+HOSTS  = os.getenv("HOST")
 PORT = os.getenv("PORT")
-KEY_SPACE = os.getenv("KEY_SPACE")
+KEYSPACE = os.getenv("KEYSPACE")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
+REPLICATION = os.getenv("REPLICATION")
+os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
 
 
 class init:
-	def __new__(cls):
-		cluster = Cluster(HOSTS, port=PORT) # db hosts and port to start clusters
-		cls.session = cluster.connect() # connect to hosts - cls is server.db.init so cls.session means self.session for init class
-		return super(init, cls).__new__(cls)
+	def __new__(cls, *args, **kwargs):
+		cls.__cluster = Cluster(HOSTS, port=PORT) # db hosts and port to start clusters
+		cls.__cluster.connection_class = LibevConnection # use libev event loop 
+		cls.__session = cls.__cluster.connect() # connect to hosts - cls is server.db.init
+		cls.__session.execute(f"CREATE KEYSPACE IF NOT EXISTS {KEYSPACE} WITH REPLICATION = {REPLICATION};")
+		cls.__session.execute(f'USE {KEYSPACE}')
+		connection.register_connection('DML', session=cls.__session) # DML cluster for sync_table ops
+		return super(init, cls).__new__(cls, *args, **kwargs)
 
   
 	def __init__(self):
-		self.session.execute(f'USE {KEY_SPACE}')
+		sync_table(User) # create users_info table if not exitscass
 		typer.secho("➲   [Database initialized successfully]", fg=typer.colors.WHITE, bold=True)
+
+
+	def query(self, q, v):
+		stmt = self.__session.prepare(q)
+		return self.__session.execute_async(stmt, v) # return a future object
