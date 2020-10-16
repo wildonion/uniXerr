03b36d1c143fbfbcd0e9b893bce098c1cb9d0ead
wildonion <marefimoghaddam@unixerr.com>
2020-10-17 00:58:30 +0330

    add user schema for users_info table

---
+++ server/db/schema/user.py
@@ -0,0 +1,28 @@
+
+
+# coding: utf-8
+
+'''
+	Codded By : 
+
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+
+
+'''
+
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+class User(Model):
+	__table_name__ = 'users_info'
+	__keyspace__ = 'unixerr'
+	__connection__ = 'DML'
+	id = columns.Integer(primary_key=True)
+	rollcall_score = columns.Float()
+	class_activity = columns.Float()
+	discipline = columns.Float()
+	total_quizzes_avg = columns.Float()
