08838c1c3663b13f5673ebc7b9ab8c56a566d8a9
wildonion <marefimoghaddam@unixerr.com>
2020-10-17 00:56:44 +0330

    complete fetch users api using both db.query() and schema model commands

--- server/api.py
+++ server/api.py
@@ -16,16 +16,16 @@
 '''
 
 from .db import init
-from .db.schema import Users, Positions
+from .db.schema import User
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel # you can use this with cassandra UserType
 import pandas as pd
 import os
 
 
 api = FastAPI()
 db = init()
-user_schema = Users(db.session)
-position_schema = Positions(db.session)
 
 
 # root of the api
@@ -34,16 +34,34 @@ async def read_root():
 	return {"Welcome to": "uniXerr"}
 
 
-# credit-info api
-@api.get("/credit-info/{user_id}")
-async def get_position(user_id: int):
-	# TODO : first insert all classified input data positions into users table with columns position_latent and position_raw
-	# 		 then fetch a classified student with his/her feature from the table
-	# ...
-	users = user_schema.get()
+
+@api.get("/users/")
+async def get_users():
+	users_list_ = []
+	users_list = []
+	future = db.query("select * from users;", []) # using db.query() method
+	users_ = future.result()
+	for user_ in users_:
+		users_list_.append(user_)
+	users = User.objects.all() # using User class model
 	for user in users:
-		print(user)
-	return {"rollcall_score": user.rollcall_score, "class_activity": user.class_activity, 
-			"discipline": user.discipline, "total_quizzes_avg": user.total_quizzes_avg, 
-			"positions": {"latent": user.position_latent, "raw": user.position_raw}}
-		  
+		users_list.append(dict(user))
+	return {"db.query()_users": users_list_, "schema_users": users_list}
+
+
+
+@api.get("/user/{user_id}")
+async def get_user():
+	future = db.query("SELECT * FROM users_info WHERE id=?", [user_id]) # using db.query() method
+	user_ = future.result()[0]
+	user = User.objects(_id=user_id)[0] # using User class model
+	return {"db.query()_user": user_, "schema_user": user}
+
+
+
+# TODO : add user api
+# https://docs.datastax.com/en/developer/python-driver/3.24/api/cassandra/cqlengine/query/
+# create a user using db.query() and User schema
+# user = User(_id=19207, rollcall_score=10, class_activity=13.66, discipline=5.99, total_quizzes_avg=6.16).save()
+# user = User.create(_id=19207, rollcall_score=10, class_activity=13.66, discipline=5.99, total_quizzes_avg=6.16)
+# ...
