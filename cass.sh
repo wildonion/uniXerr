c05dd6c25dd4ee0c1e704d8af301d15d35cfdeb1
wildonion <marefimoghaddam@unixerr.com>
2020-10-23 08:23:05 +0330

    this script must be completed on server

--- cass.sh
+++ cass.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-# cassandra snapshot script
+# cassandra snapshot and export tables script using crontab
+# exported tables must be in server/db/_exported folder
 # https://www.geeksforgeeks.org/data-backup-and-restoration-in-cassandra/?ref=rp
