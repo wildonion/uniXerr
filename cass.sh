d1923bee496ae6a6964a171e594f975fe73d395a
wildonion <marefimoghaddam@unixerr.com>
2020-10-24 23:21:12 +0330

    remove cass.sh file

--- cass.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-# cassandra snapshot and export tables script using crontab
-# exported tables must be in server/db/_exported folder
-# https://www.geeksforgeeks.org/data-backup-and-restoration-in-cassandra/?ref=rp
