9c67a3671150f6c437f2b7f7e14dd6e40dddc4cd
2017-A-15 <marefimoghaddam@unixerr.com>
2021-03-29 16:06:58 +0430

    add some docker commands

--- app.py
+++ app.py
@@ -45,34 +45,41 @@ def deploy(build: bool = typer.Option(False, "--build", help="Build from docker-
 		# TODO : update README.md with docker-compose commands after building containers
                 # TODO : secure api.py inside server folder using https://fastapi.tiangolo.com/tutorial/security/ and build it using docker-compose.yml, traefik and other DevOps tools (https://fastapi.tiangolo.com/deployment/) 
                 '''
-                         ____________________________
-                         SOME DOCKER EXAMPLE COMMANDS 
-                         ----------------------------
-                  sudo docker-compose inspect <SERVICE_NAME_IN_DOCKER_COMPOSE>
-                  sudo docker-compose -f docker-compose.yml up -d --build
-                  sudo docker-compose run <SERVICE_NAME_IN_DOCKER_COMPOSE> bash
-                  sudo docker-compose restart <SERVICE_NAME_IN_DOCKER_COMPOSE>
-                  sudo docker volume inspect inobi_media_volume
-                  sudo docker-compose -f docker-compose.yml logs -f
-                  sudo docker-compose down -v
-                  sudo docker-compose -f docker-compose.yml up --build
-                  sudo docker-compose exec db psql --username=inobi --dbname=inobi < inobi.sql
-                  sudo docker-compose exec db psql --username=traccar --dbname=traccar < traccar.sql
-                  sudo docker build -t inobi .
-                  sudo docker run -it inobi /bin/bash
-                  sudo docker run -d -it -p 8586:8586 inobi
-                  sudo docker images
-                  sudo docker volume ls
-                  sudo docker commit [CONTAINER_ID] [new_image_name]
-                  sudo docker stop <CONTAINER/IMAGE_NAME/ID>
-                  sudo docker rmi -f <CONTAINER/IMAGE_NAME/ID>
-                  sudo docker image prune -a
-                  sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
-                  sudo docker rm -f $(sudo docker ps -aq)
-                  sudo docker ps
-                  sudo docker login --username=wildonion --password="password"
-                  sudo docker commit <CONTAINER_ID> inobi
-                  sudo docker cp /home/mehran/inobi/  e4d47a395d07:/home/aranobi/
+					 ____________________________
+					 SOME DOCKER EXAMPLE COMMANDS 
+					 ----------------------------
+                  	sudo docker network connect <NETWORK_NAME> <CONTAINER/IMAGE_NAME/ID>
+			sudo docker network create -o com.docker.network.bridge.enable_icc=true -d bridge <NETWORK_NAME>
+			sudo docker network ls
+			sudo docker network inspect -f '{{range .Containers}}{{.Name}} {{end}}' <NETWORK_NAME>
+			-------------------------------------------------------------------------------------------------------------
+			sudo docker-compose -f docker-compose.yml build --no-cache && sudo docker-compose up -d --force-recreate
+			sudo docker-compose run <SERVICE_NAME_IN_DOCKER_COMPOSE> bash
+			sudo docker-compose restart <SERVICE_NAME_IN_DOCKER_COMPOSE>
+			sudo docker-compose -f docker-compose.yml logs -f
+			sudo docker-compose down -v
+			sudo docker-compose -f docker-compose.yml up --build
+			sudo docker-compose exec db psql --username=uniXerr --dbname=uniXerr < uniXerr.sql
+			-------------------------------------------------------------------------------------------------------------
+			sudo docker ps
+			sudo docker exec <CONTAINER/IMAGE_NAME/ID>_A ping <CONTAINER/IMAGE_NAME/ID>_B -c2
+			sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <CONTAINER_ID>
+			sudo docker inspect -f '{{index .Options "com.docker.network.bridge.enable_icc"}}' <NETWORK_NAME>
+			sudo docker build -t uniXerr .
+			sudo docker run -it uniXerr /bin/bash
+			sudo docker run -d -it -p 8586:8586 uniXerr --network=<NETWORK_NAME>
+			sudo docker images
+			sudo docker volume ls
+			sudo docker volume inspect <CHOOSE_ONE_FROM_ABOVE_COMMAND>
+			sudo docker commit <CONTAINER/IMAGE_NAME/ID> <NEW_IMAGE_NAME>
+			sudo docker stop <CONTAINER/IMAGE_NAME/ID>
+			sudo docker rmi -f <CONTAINER/IMAGE_NAME/ID>
+			sudo docker image prune -a
+			sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
+			sudo docker rm -f $(sudo docker ps -aq)
+			sudo docker login --username=wildonion --password="password"
+			sudo docker commit <CONTAINER/IMAGE_NAME/ID> uniXerr
+			sudo docker cp /home/wildonion/uniXerr/  e4d47a395d07:/home/wildonion/
                '''
                # ... 
 
