5f7ba1662865d023071aaca650b660de9bd4e519
2017-A-15 <marefimoghaddam@unixerr.com>
2021-04-10 08:08:16 +0430

    add docker build commands

--- launch.sh
+++ launch.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 
-#    				 _______________________________________________
-# 					|	      SOME DOCKER EXAMPLE COMMANDS          |
-# 					|containers are reachable by their service names|
-# 	 				 -----------------------------------------------
-# 
+
+# 📌 CMD and ENTRYPOINT in Dockerfile will execute the command first and then stop the container with exit code 0 (no error)
+# 📌 we can't have multiple CMD or ENTRYPOINT in our Dockerfile 
+# 📌 containers are reachable by their service names
+
+
+
+
 # sudo docker network connect <NETWORK_NAME> <CONTAINER/IMAGE_NAME/ID>
 # sudo docker network create -o com.docker.network.bridge.enable_icc=true -d bridge <NETWORK_NAME>
 # sudo docker network ls
@@ -13,7 +16,7 @@
 # -------------------------------------------------------------------------------------------------------------
 # sudo docker-compose -f docker-compose.yml build --no-cache
 # sudo docker-compose up -d --force-recreate
-# sudo docker-compose -f docker-compose.yml logs -f
+# sudo docker-compose -f docker-compose.yml logs
 # sudo docker-compose run -u aranobi <SERVICE_NAME_IN_DOCKER_COMPOSE> bash
 # sudo docker-compose restart <SERVICE_NAME_IN_DOCKER_COMPOSE>
 # sudo docker-compose down -v
@@ -22,8 +25,8 @@
 # -------------------------------------------------------------------------------------------------------------
 # sudo docker save $(sudo docker images -q) -o docker-utils/uniXerr.tar
 # sudo docker load -i -o docker-utils/uniXerr.tar
-# sudo docker save $(docker images -q) | gzip > inobi.tar.gz
-# sudo docker load -i inobi.tar.gz
+# sudo docker save $(sudo docker images -q) | gzip > uniXerr.tar.gz
+# sudo docker load -i uniXerr.tar.gz
 # sudo docker ps
 # sudo docker exec <CONTAINER/IMAGE_NAME/ID>_A ping <CONTAINER/IMAGE_NAME/ID>_B -c2
 # sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <CONTAINER_ID>
@@ -47,3 +50,23 @@
 # sudo docker commit <CONTAINER/IMAGE_NAME/ID> uniXerr
 # sudo docker cp /home/wildonion/uniXerr/  e4d47a395d07:/home/wildonion/
 # sudo docker cp 4ba0d2853dd2:/opt/uniXerr/migrations /home/wildonion/utils
+
+
+
+
+
+echo "⌛ Stopping and Removing all containers..."
+sudo docker stop $(sudo docker ps -a -q) && sudo docker-compose down -v
+echo "⌛ Pruning system..."
+sudo docker system prune -af --volumes
+echo "⌛ Building images with --no-cache enabled..."
+sudo docker-compose -f docker-compose.yml build --no-cache
+echo "⌛ Running all containers from built images with --force-recreate enabled..."
+sudo docker-compose up -d --force-recreate
+echo "⌛ Showing containers log..."
+sudo docker-compose -f docker-compose.yml logs -f
+
+
+
+
+
