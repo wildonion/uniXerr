9d00976b5aa171eaacc73cec0feb7b19ef56a18e
2017-A-15 <marefimoghaddam@unixerr.com>
2021-04-10 09:01:26 +0430

    add some docker commands

--- launch.sh
+++ launch.sh
@@ -8,6 +8,37 @@
 
 
 
+# ==============
+# first building
+# ==============
+# ✅ sudo docker stop $(sudo docker ps -a -q) && sudo docker-compose down -v && sudo docker system prune -af --volumes
+# ✅ sudo docker-compose -f docker-compose.yml build --no-cache && sudo docker-compose up -d --force-recreate
+# ✅ sudo docker-compose -f docker-compose.yml logs
+
+
+
+
+# =================
+# on changing build
+# =================
+# ✅ sudo docker stop $(sudo docker ps -a -q)                               --------------------------------> stop all running containers
+# ✅ sudo docker-compose -f docker-compose.yml build --no-cache             --------------------------------> update images, omit the --no-cache if you want to use cache building
+# ✅ sudo docker-compose down -v && sudo docker-compose up --force-recreate --------------------------------> remove and rebuild all containers, you will lose the old ones data
+# ✅ sudo docker-compose up -d --force-recreate                             --------------------------------> omit the --force-recreate if you don't want to recreate all the containers
+# ✅ sudo docker-compose -f docker-compose.yml logs                         --------------------------------> see the docker containers logs
+# ✅ sudo docker-compose run -u aranobi web bash                            --------------------------------> accessing bash shell of we service
+
+
+
+
+# ========================================
+# saving and loading images on other hosts
+# ========================================
+# ✅ sudo docker save $(sudo docker images -q) | gzip > uniXerr.tar.gz
+# ✅ sudo docker load -i -o uniXerr.tar
+
+
+
 
 # sudo docker network connect <NETWORK_NAME> <CONTAINER/IMAGE_NAME/ID>
 # sudo docker network create -o com.docker.network.bridge.enable_icc=true -d bridge <NETWORK_NAME>
