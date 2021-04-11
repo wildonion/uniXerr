7fd6e086f357ae4f2046e292580000fb9bbc50b9
2017-A-15 <marefimoghaddam@unixerr.com>
2021-04-11 16:29:53 +0430

    update a docker commands

--- launch.sh
+++ launch.sh
@@ -26,7 +26,7 @@
 # ✅ sudo docker-compose down -v && sudo docker-compose up --force-recreate --------------------------------> remove and rebuild all containers, you will lose the old ones data
 # ✅ sudo docker-compose up -d --force-recreate                             --------------------------------> omit the --force-recreate if you don't want to recreate all the containers
 # ✅ sudo docker-compose -f docker-compose.yml logs                         --------------------------------> see the docker containers logs
-# ✅ sudo docker-compose run -u uniXerr web bash                            --------------------------------> accessing bash shell of we service
+# ✅ sudo docker-compose run -u <SERVICE_NAME> web bash                     --------------------------------> accessing bash shell of a service
 
 
 
