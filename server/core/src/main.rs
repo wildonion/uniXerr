498e7d3479b3049e6edfdf84f15730a653c4c6c9
user29 <erfan@araglobal.ir>
2021-04-11 14:01:07 +0430

    add core servers

---
+++ server/core/src/main.rs
@@ -0,0 +1,13 @@
+
+
+
+
+// https://github.com/telegram-rs/telegram-bot
+// https://github.com/teloxide/teloxide
+
+// server for:
+// pre-trained core AI models
+// uPC telegram bot in rust
+
+// tools:
+// tch, tokio, actix, rocket, hyper, zmq, docker and k8s based on streaming async paradigm
