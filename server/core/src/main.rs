9b1485d92bd612c14802aeb3c0b5d98c3320cdbf
2017-A-15 <marefimoghaddam@unixerr.com>
2021-04-11 17:42:37 +0430

    add refs

--- server/core/src/main.rs
+++ server/core/src/main.rs
@@ -4,10 +4,14 @@
 
 // https://github.com/telegram-rs/telegram-bot
 // https://github.com/teloxide/teloxide
+// https://rust-lang.github.io/rustup/index.html
+// https://www.rust-lang.org/learn
 
+// ============================================
 // server for:
 // pre-trained core AI models
 // uPC telegram bot
 
 // tools:
 // tch, tokio, actix, rocket, hyper, zmq, docker and k8s based on streaming async paradigm
+// ============================================
