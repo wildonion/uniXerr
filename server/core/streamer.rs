779bf2883cf301b11fa396bfb0cd4cd815e75e9a
2017-A-15 <marefimoghaddam@unixerr.com>
2021-05-27 19:44:56 +0430

    add streamer producer

---
+++ server/core/streamer.rs
@@ -0,0 +1,41 @@
+
+
+'''
+	Codded By : 
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+ -----------------------------------------------------------------------------------
+| features are sent weekly to the uniXerr server for prediction over kafka streaming
+| the model is always training, this means that the features are sent from the app 
+| constantly at every moment to the server at any time for training.
+| __________________________________________________________________________________
+|
+|
+| https://www.confluent.io/blog/using-apache-kafka-drive-cutting-edge-machine-learning/
+| https://faust.readthedocs.io/en/latest/
+| https://github.com/kkroening/ffmpeg-python
+| https://kafka-python.readthedocs.io/en/master/usage.html
+| https://aroussi.com/post/live-plotting-with-matplotlib-and-python
+| https://medium.com/analytics-vidhya/data-streams-and-online-machine-learning-in-python-a382e9e8d06a
+| https://www.confluent.io/blog/improving-stream-data-quality-with-protobuf-schema-validation/
+| https://medium.com/data-rocks/protobuf-as-an-encoding-format-for-apache-kafka-cad4709a668d
+| https://www.kai-waehner.de/blog/2018/02/13/machine-learning-trends-of-2018-combined-with-apache-kafka-ecosystem/?sfw=pass1593361971
+| https://itnext.io/getting-started-with-kafka-and-rust-part-1-e0074961ec6b
+| https://itnext.io/getting-started-with-kafka-and-rust-part-1-e0074961ec6b
+| https://itnext.io/getting-started-with-kafka-and-rust-part-2-129c372dc1ad
+| https://www.confluent.io/blog/getting-started-with-rust-and-kafka/
+| https://medium.com/digitalfrontiers/microservices-in-rust-with-kafka-2b671295b24ehttps://itnext.io/getting-started-with-kafka-and-rust-part-2-129c372dc1ad
+| https://www.confluent.io/blog/getting-started-with-rust-and-kafka/
+| https://medium.com/digitalfrontiers/microservices-in-rust-with-kafka-2b671295b24e
+|
+|
+'''
+
+
+
+
+
+
