4a97bd68c1a9efc637fc4a9d187070e6c22060ec
wildonion <marefimoghaddam@unixerr.com>
2021-10-11 17:26:52 +0330

    + add a union based GPSData structure

--- server/skeleton/microservices/tracer/src/schemas/gsp.rs
+++ server/skeleton/microservices/tracer/src/schemas/gsp.rs
@@ -1,12 +1,6 @@
 
 
 
-
-
-
-
-
-
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
@@ -14,9 +8,22 @@ use uuid::Uuid;
 
 
 
+// -----------------------------------------------------------------------------------------------
+// in order to prevent from null checking we have to take a safe rusty based reference to GPSData 
+// inside the Option (cause rust doesn't have null) cause *mut raw pointer to GPSMem inside 
+// union is not safe also can't move between thread safely and once the GPS data has dereferenced 
+// it might return a null pointer due to unsafe manner of C pointers.
+// -----------------------------------------------------------------------------------------------
+#[repr(C)] //-- #[repr(C, packed)] : borrow of packed field is unsafe and requires unsafe function or block
+union GPSData{
+    pub data: *mut self::GPSMem,
+    pub buffer: *const u8,
+}
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct MetaData{
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+#[repr(C)]
+pub struct GPSMem{
     pub id: Uuid,
     pub time: i64,
 }
@@ -26,9 +33,9 @@ pub struct MetaData{
 
 
 
-impl Default for MetaData{
+impl Default for GPSMem{
     fn default() -> Self{
-        MetaData{
+        GPSMem{
             id: Uuid::new_v4(),
             time: chrono::Local::now().naive_local().timestamp(),
         }
