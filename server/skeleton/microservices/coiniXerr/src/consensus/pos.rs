b8a87bd233c2425b4a49e4496cf55c26ec62a21b
wildonion <marefimoghaddam@unixerr.com>
2021-10-16 17:40:14 +0330

    update to the latest changes of the project

--- server/skeleton/microservices/coiniXerr/src/consensus/pos.rs
+++ server/skeleton/microservices/coiniXerr/src/consensus/pos.rs
@@ -1,10 +1,10 @@
 
 
 
-
 // https://www.investopedia.com/terms/p/proof-stake-pos.asp
 // TODO - to issue a smart contract the consensus algorithm or mining process must be done first
 // TODO - after mining process has completed a smart contract will be issued to sign each transaction
 // TODO - finally each smart contract will be added to the coiniXerr blockchain
 // NOTE - peer can have a successful coin transfer process once a smart contract is added to the blockchain
+// NOTE - public key is used to generate wallet address and private key is used to sign blockchain based transactions
 // ...
