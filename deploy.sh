b10bf31658129eea8c20443fedecbec52367f86e
2017-A-15 <marefimoghaddam@unixerr.com>
2021-10-18 08:47:36 +0330

    remove psychoder setup cause it’s a cli framework :))

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@
 # ...
 # cargo build --bin auth --release --manifest-path server/skeleton/microservices/Cargo.toml
 # cargo build --bin suproxy --release --manifest-path server/skeleton/microservices/Cargo.toml
-# cargo build --bin psychoder --release --manifest-path server/skeleton/microservices/Cargo.toml
 # cargo build --bin coiniXerr --release --manifest-path server/skeleton/microservices/Cargo.toml
 
 
@@ -25,11 +24,9 @@ diesel setup --migration-dir auth/migrations/
 chown -R $USER:$USER . && cargo build --bins --release --manifest-path Cargo.toml && sudo cp .env $HONE/.env
 sudo mv target/release/auth $HONE
 sudo mv target/release/suproxy $HONE
-sudo mv target/release/psychoder $HONE
 sudo mv target/release/coiniXerr $HONE
 cd $HOME
 pm2 start auth
 pm2 start suproxy
-pm2 start psychoder
 pm2 start coiniXerr
 pm2 startup
