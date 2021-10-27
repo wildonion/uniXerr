822cd1b87f81672a6884a04d4dd83d238d8e52f6
wildonion <marefimoghaddam@unixerr.com>
2021-10-27 16:39:23 +0330

    update to the latest changes

--- server/skeleton/microservices/coiniXerr/src/peer/kafcriber.rs
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-// NOTE - https://github.com/lucrussell/kafka-blockchain
-// NOTE - stream over incoming transactions from each kafka subscriber to mine and add them to the blockchain using coiniXerr consensus algorithm
-// ...
