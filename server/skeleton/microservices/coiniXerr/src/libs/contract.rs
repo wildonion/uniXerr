5297b80049054fb2738819734fc92221bde22867
2017-A-15 <marefimoghaddam@unixerr.com>
2021-11-03 12:27:48 +0330

    Create contract.rs

---
+++ server/skeleton/microservices/coiniXerr/src/libs/contract.rs
@@ -0,0 +1,8 @@
+
+
+
+
+
+// https://medium.com/block-journal/introducing-substrate-smart-contracts-with-ink-d486289e2b59
+// substrate blockchain framework with its smart contract language ink!
+// ...
