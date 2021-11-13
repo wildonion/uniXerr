701f12348f2de7e4f073e7c48c608c520389e23c
2017-A-15 <marefimoghaddam@unixerr.com>
2021-11-13 16:35:49 +0330

    Update pos.rs

--- server/skeleton/microservices/coiniXerr/src/cvm/consensus/pos.rs
+++ server/skeleton/microservices/coiniXerr/src/cvm/consensus/pos.rs
@@ -12,7 +12,7 @@ use crate::schemas::Block;
 
 pub fn can_be_mined(mut block: Block) -> Result<Block, bool>{ //-- define block as mutable cause we want to update its is_mined field
     println!("-> mining process has been started for block [{}]", block.id);
-    // TODO - confirm all valid transactions (mempool) inside a block using a proof of stake consensus algorithm based on uniXerr peers' features and coins and VAE algorithm
+    // TODO - confirm all valid transactions (mempool) inside a block using a proof of stake or NFA and DFA based consensus algorithm based on uniXerr peers' features and coins and VAE algorithm
     // TODO - an encoder in the client side is responsible to encode each transaction
     // TODO - all encoded transactions will be stored inside a block inside the server cause the're valid transactions (mempool)
     // TODO - a decoder in the server side is responsible to decode all of them from the latent space of VAE
