ace2681aec6b8feb0cdb22edb4240bc85fa28a8b
wildonion <marefimoghaddam@unixerr.com>
2021-11-15 18:32:54 +0330

    add note for sending message to miner actor

--- server/skeleton/microservices/coiniXerr/src/main.rs
+++ server/skeleton/microservices/coiniXerr/src/main.rs
@@ -299,6 +299,8 @@ async fn main() -> std::io::Result<()>{
     /////// ==========--------------==========--------------==========--------------==========--------------==========--------------
     while let Some(transaction) = transaction_receiver.recv().await{ //-- waiting for each transaction to become available to the down side of channel (receiver) for mining process cause sending is done asynchronously 
         println!("-> receiving new transaction to push inside the current block");
+        // TODO - check the type of the incoming transaction and send issue contract message to miner actor if the transaction was of type smart contract or 0xFF
+        // ...
         let mutext_transaction = transaction.lock().unwrap().clone();
         // ----------------------------------------------------------------------
         //                              MINING PROCESS
