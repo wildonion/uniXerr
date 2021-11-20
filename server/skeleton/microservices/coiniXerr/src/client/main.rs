7b43ddc6b95d374d1b095249f94f272dfc858283
2017-A-15 <marefimoghaddam@unixerr.com>
2021-11-20 09:11:11 +0330

    Update main.rs

--- server/skeleton/microservices/coiniXerr/src/client/main.rs
+++ server/skeleton/microservices/coiniXerr/src/client/main.rs
@@ -20,7 +20,7 @@ async fn main() -> std::io::Result<()>{ //-- await is only allowd inside an asyn
     ///////                                          parachains and parathreads coiniXerr node
     /////// ==========--------------==========--------------==========--------------==========--------------==========--------------
     // NOTE - nodes' weights are their stakes 
-    // NOTE - web3 or polkadotjs <-grpc or wss api-> blockchain nodes
+    // NOTE - web3 or polkadotjs <-grpc or wss api-> store some transaction based assets in each block of all blockchain nodes
     // TODO - connect other coiniXerr full node blockchains from other instances of this server to build a parachains and parathreads based network for parallel transactions using scheduler, libp2p, protobuf and gRPC protocol over http or tcp socket
     // TODO - solve forking and reorgs issue for this model of blockchain by choosing the longest chain created by new() method of the blockchain object of the Chain struct 
     // TODO - even though it's possible for two rivaling chains to exist at the same time, soon one of the two chains will add another block and outgrow the other due to the time it takes to solve the mining algorithms,
