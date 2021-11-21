41ff399b1fb6db508e873c7af47fa20318c3f053
2017-A-15 <marefimoghaddam@unixerr.com>
2021-11-21 09:30:56 +0330

    Update main.rs

--- server/skeleton/microservices/coiniXerr/src/client/src/main.rs
+++ server/skeleton/microservices/coiniXerr/src/client/src/main.rs
@@ -21,12 +21,12 @@ async fn main() -> std::io::Result<()>{ //-- await is only allowd inside an asyn
     /////// ==========--------------==========--------------==========--------------==========--------------==========--------------
     // NOTE - nodes' weights are their stakes which is their total coiniXerr balance
     // TODO - solve forking and reorgs issue for this model of blockchain by choosing the longest chain created by new() method of the blockchain object of the Chain struct 
-    // TODO - save the whole chain state inside a db or a persistence storage
+    // TODO - save the whole chain state inside a db or a persistence storage in every peer of coiniXerr blockchain node
     // ...
     
     
     // TEST - web3 or polkadotjs <-p2p based gRPC or IPC or ws network-> store some transaction based assets in all blockchain nodes
-    // TEST - run .wasm in js for calling jsonRPC web3 over http and ws
+    // TEST - run .wasm to call coiniXerr blockchain network methods and functions directly from js 
 
 
     Ok(())
