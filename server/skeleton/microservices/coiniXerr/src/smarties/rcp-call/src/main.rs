be00b3b0201ed6e599ff34e3e9585c0e8b232c41
2017-A-15 <marefimoghaddam@unixerr.com>
2021-12-27 18:06:33 +0330

    add hyper crate

--- server/skeleton/microservices/coiniXerr/src/smarties/rcp-call/src/main.rs
+++ server/skeleton/microservices/coiniXerr/src/smarties/rcp-call/src/main.rs
@@ -5,10 +5,20 @@
 
 
 
+
+
+
+
+
+
+
 fn main(){
 
-
-    // an RPC call to our smart contract by seding a new transaction to our deployed contract_program to change its state using defined instructions and the instruction_data field  
+    // https://docs.rs/hyper/0.14.16/hyper/server/index.html
+    // http://zderadicka.eu/hyper-websocket/
+    // an RPC call to our smart contract by seding a new transaction to our deployed contract_program to change its state using defined instructions and the instruction_data field
+    // TODO - add features to a module to include it in Cargo when installing that module like { version = "0.14", features = ["full"] }
+    // TODO - design pattern syntax for api calls ====> api!(endpoint, data, access(2), method)
     // ...
 
 }
