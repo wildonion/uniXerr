4d7ddc17c77746e6cd3a17433945d48aa5edf0ee
2017-A-15 <marefimoghaddam@unixerr.com>
2021-12-29 14:36:23 +0330

    !!!

--- server/skeleton/microservices/coiniXerr/src/smarties/rcp-call/src/main.rs
+++ server/skeleton/microservices/coiniXerr/src/smarties/rcp-call/src/main.rs
@@ -19,14 +19,11 @@ impl Debug for ! {
 
 
 
+
+
+use std::{convert::Infallible, net::SocketAddr};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn}; //-- required for making a service from a function for the hyper server
-use std::{
-          //-- The error type for errors that can never happen, since this enum has no variant, a value of this type can never actually exist. 
-          //-- this can be useful for generic APIs that use [Result] and parameterize the error type, to indicate that the result is always [Ok] 
-          convert::Infallible, 
-          net::SocketAddr
-        };
 
 
 
@@ -34,6 +31,8 @@ use std::{
 
 
 
+//-- The error type for errors that can never happen, since this enum has no variant, a value of this type can never actually exist. 
+//-- this can be useful for generic APIs that use [Result] and parameterize the error type, to indicate that the result is always [Ok] 
 pub type Empty = ();
 pub type Infallible = !; //-- define the associated type of never type using Infallible - Infallible and ! are the same type and expressions with type ! will coerce into any other type
 pub type Error = Infallible; //-- an error which never happen
@@ -60,15 +59,12 @@ async fn main(){
     // https://docs.rs/hyper/0.14.16/hyper/server/index.html
     // http://zderadicka.eu/hyper-websocket/
     // an RPC call to our smart contract by seding a new transaction to our deployed contract_program to change its state using defined instructions and the instruction_data field
+    // TODO - all incoming binary streaming RPC commands will be decoded in here using the hyper http server
     // TODO - design pattern syntax for api calls ====> api!(endpoint, data, access(2), method)
     // ...
     
 
 
-
-
-
-    
     let x: ! = {
         return 123; //-- here ! will be coerced to i32 
     };
@@ -78,8 +74,11 @@ async fn main(){
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let make_svc = make_service_fn(|_conn| async{
-        Ok::<_, Infallible>(service_fn(handle))
+        println!("inside the service function..");
+        Ok::<_, Infallible>(service_fn(handle)) //-- it'll never return error on Ok cause we've used Infallible type
     });
+
+
 
     let server = Server::bind(&addr).serve(make_svc);
     if let Err(e) = server.await{
