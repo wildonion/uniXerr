1b7ab25e1a884c952f6ec6e727721fed355b60ba
2017-A-15 <marefimoghaddam@unixerr.com>
2022-08-06 14:38:39 +0430

    Update mathista.rs

--- core/psychoder/src/mathista.rs
+++ core/psychoder/src/mathista.rs
@@ -7,6 +7,8 @@
 // https://github.com/LaurentMazare/tch-rs
 // https://nalgebra.org/
 // https://drive.google.com/drive/folders/1elbMIrg5_NlNMzrKAX_KNjJzgD-MvNQd
+// https://drive.google.com/file/d/1n_7VZtBk5Vy_RLyb2IXNtWKurZ9NSwyn/view?usp=sharing
+
 
 
 pub mod tensor;
