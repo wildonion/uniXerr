f64c86ac2f93206c7eecd1eddae4a86669005bfd
ONIONTOR <marefimoghaddam@unixerr.com>
2022-09-14 14:22:26 +0430

    add more links

--- core/psychoder/src/mathista.rs
+++ core/psychoder/src/mathista.rs
@@ -2,6 +2,16 @@
 
 
 
+
+
+// https://crates.io/crates/mnist
+// https://crates.io/crates/wasi-nn
+// https://crates.io/crates/wasmtime-wasi-nn
+// https://ngoldbaum.github.io/posts/python-vs-rust-nn/
+// https://towardsdatascience.com/machine-learning-and-rust-part-4-neural-networks-in-torch-85ee623f87a
+// https://crates.io/crates/rusty-machine
+// https://crates.io/crates/nd_array
+// https://crates.io/crates/ndarray-linalg
 // https://www.learnpytorch.io/
 // https://pytorch.org
 // https://github.com/LaurentMazare/tch-rs
