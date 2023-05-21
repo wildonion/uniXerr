b0aba8b6e39c8f4d3311638bc57dcc7bc11d47be
dewo <ea_pain@yahoo.com>
2023-05-21 13:49:31 +0330

    add contract macro

--- infra/valhalla/coiniXerr/src/utils.rs
+++ infra/valhalla/coiniXerr/src/utils.rs
@@ -1188,3 +1188,92 @@ example!();
 //       new_z "🟥": [Pos(0, 0), Pos(-1, 0), Pos(0, 1), Pos(1, 1)] @ Pos(0, 0);
 //     }
 // }
+    
+    
+ #[macro_export]
+macro_rules! contract {
+
+    /*
+
+        contract!{
+
+            NftContract, //// name of the contract
+            "wildonion.near", //// the contract owner
+            /////////////////////
+            //// contract fields
+            /////////////////////
+            [
+                contract_owner: AccountId, 
+                deposit_by_owner: HashMap<AccountId, near_sdk::json_types::U128>, 
+                contract_balance: near_sdk::json_types::U128
+            ]; //// fields
+            /////////////////////
+            //// contract methods
+            /////////////////////
+            [ 
+                "init" => [ //// array of init methods
+                    pub fn init_contract(){
+            
+                    }
+                ],
+                "private" => [ //// array of private methods
+                    pub fn get_all_deposits(){
+
+                    }
+                ],
+                "payable" => [ //// array of payable methods
+                    pub fn deposit(){
+            
+                    }
+                ],
+                "external" => [ //// array of external methods
+                    fn get_address_bytes(){
+
+                    }
+                ]
+            ]
+
+        }
+
+    */
+
+    // event!{
+    //     name: "list_owner",
+    //     log: [NewOwner, AddDeposit],
+
+    //     // event methods
+
+    //     fn add_owner(){
+
+    //     } 
+
+    //     fn add_deposit(){
+            
+    //     }
+    // }
+
+    // emit!{
+    //     event_name
+    // }
+
+    (
+     $name:ident, $signer:expr, //// ident can be used to pass struct
+     [$($fields:ident: $type:ty),*]; 
+     [$($method_type:expr => [$($method:item),*]),* ]
+    ) 
+     
+     => {
+            #[near_bindgen]
+            #[derive(serde::Deserialize, serde::Serialize)]
+            pub struct $name{
+                $($fields: $type),*
+            }
+
+            impl $name{
+                        
+                // https://stackoverflow.com/questions/64790850/how-do-i-write-a-macro-that-returns-the-implemented-method-of-a-struct-based-on
+                // TODO - implement methods here 
+                // ...
+            }
+    }
+}
